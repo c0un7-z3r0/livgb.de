cc55fbf7415c5871f9a7952987d58498bd503f3a
David Kagerer <david@kagerer.co>
2024-05-11 12:33:14 +0200

    fix: correct image url mapping

--- src/lib/image.helper.ts
+++ src/lib/image.helper.ts
@@ -1,5 +1,4 @@
 import type { ImageFragment } from "../graphql/__generated__/operations";
-import { importImage } from "astro-imagetools/api";
 import { logger } from "./logger";
 
 export const getFileAttributes = (file: ImageFragment) => {
@@ -11,40 +10,40 @@ export const getImageUrl = (file: ImageFragment) => {
 };
 
 export interface MappedImage {
-  url: string;
+  source: { height: number; width: number; url: string };
   alternativeText: string;
-  preview: string;
-  large: { height: string; width: string; url: string };
+  preview: { height: number; width: number; url: string };
+  large: { height: number; width: number; url: string };
 }
 export const buildImageUrl = (path: string) =>
   `${import.meta.env.STRAPI_URL}${path}`;
-export async function mapImages(
-  imageList: ImageFragment[],
-): Promise<MappedImage[]> {
-  logger.info("Mapping images for image gallery", { total: imageList.length });
-  return Promise.all(
-    imageList.map(async (i) => {
-      const attr = getFileAttributes(i);
-      const sourceUrl = getImageUrl(i);
 
-      const largeImage = {
-        height: attr.formats.large?.height || attr.height,
-        width: attr.formats.large?.width || attr.width,
-        url: await importImage(
-          buildImageUrl(attr.formats.large?.url || attr.url),
-        ),
-      };
-      return {
-        url: sourceUrl,
-        alternativeText: getFileAttributes(i).alternativeText,
-        preview: attr.formats.medium
-          ? buildImageUrl(attr.formats.medium.url)
-          : sourceUrl,
-        large: largeImage,
-      };
-    }),
-  ).catch((err) => {
-    logger.error("Failed to map images", { error: err.message });
-    return [];
+export function mapImages(imageList: ImageFragment[]): MappedImage[] {
+  logger.info("Mapping images for image gallery", { total: imageList.length });
+  return imageList.map((image) => {
+    const attr = getFileAttributes(image);
+
+    const largeImage = {
+      height: attr.formats.large?.height || attr.height,
+      width: attr.formats.large?.width || attr.width,
+      url: buildImageUrl(attr.formats.large?.url || attr.url),
+    };
+    const previewImage = {
+      height: attr.formats.medium?.height || attr.height,
+      width: attr.formats.medium?.width || attr.width,
+      url: buildImageUrl(attr.formats.medium?.url || attr.url),
+    };
+    const sourceImage = {
+      height: attr.height,
+      width: attr.width,
+      url: buildImageUrl(attr.url),
+    };
+
+    return {
+      source: sourceImage,
+      alternativeText: getFileAttributes(image).alternativeText,
+      preview: previewImage,
+      large: largeImage,
+    };
   });
 }
