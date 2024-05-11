6d8c3092a2a56a7c8297455f64912791b8c47c18
David Kagerer <david@kagerer.co>
2024-05-11 09:47:01 +0200

    feat: add photoswipe as image gallery
    https://photoswipe.com/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,26 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+import partytown from "@astrojs/partytown";
+import playformCompress from "@playform/compress";
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      svgoOptions: {},
-    }),
-    astroImageTools,
-  ],
+  site: "https://livgb.de",
+  integrations: [tailwind(), icon({
+    svgoOptions: {}
+  }), astroImageTools, robotsTxt({
+    sitemap: false
+  }), playformCompress(), playformInline()],
   output: "server",
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
   adapter: node({
-    mode: "middleware",
-  }),
+    mode: "middleware"
+  })
 });
