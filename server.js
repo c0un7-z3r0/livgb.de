db1a09db04e25890808d20c79ff1f40abed8b662
David Kagerer <david@kagerer.co>
2024-05-11 11:33:17 +0200

    feat: add winston logger

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({
-      filename: "logs/debug.log",
+      filename: "logs/server.log",
     }),
   ],
 });
