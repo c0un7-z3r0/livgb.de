35cd683ca6038b56207f7f0b564d9d58d9bfb45e
David K <david@kagerer.co>
2024-05-12 19:04:15 +0000

    feat: add photoswipe gallery (#6)
    * fix: add backdrop to close mobile menu (#4)
    
    * feat: add photoswipe gallery (#5)
    
    * feat: add photoswipe
    
    * feat: add gallery test page
    
    * fix: try to get image gallery work again

--- src/lib/image.helper.ts
+++ src/lib/image.helper.ts
@@ -1,4 +1,5 @@
 import type { ImageFragment } from "../graphql/__generated__/operations";
+import { logger } from "./logger";
 
 export const getFileAttributes = (file: ImageFragment) => {
   return file.File.data.attributes;
@@ -8,5 +9,41 @@ export const getImageUrl = (file: ImageFragment) => {
   return buildImageUrl(url);
 };
 
+export interface MappedImage {
+  source: { height: number; width: number; url: string };
+  alternativeText: string;
+  preview: { height: number; width: number; url: string };
+  large: { height: number; width: number; url: string };
+}
 export const buildImageUrl = (path: string) =>
   `${import.meta.env.STRAPI_URL}${path}`;
+
+export function mapImages(imageList: ImageFragment[]): MappedImage[] {
+  logger.info("Mapping images for image gallery", { total: imageList.length });
+  return imageList.map((image) => {
+    const attr = getFileAttributes(image);
+
+    const largeImage = {
+      height: attr.formats.large?.height || attr.height,
+      width: attr.formats.large?.width || attr.width,
+      url: buildImageUrl(attr.formats.large?.url || attr.url),
+    };
+    const previewImage = {
+      height: attr.formats.medium?.height || attr.height,
+      width: attr.formats.medium?.width || attr.width,
+      url: buildImageUrl(attr.formats.medium?.url || attr.url),
+    };
+    const sourceImage = {
+      height: attr.height,
+      width: attr.width,
+      url: buildImageUrl(attr.url),
+    };
+
+    return {
+      source: sourceImage,
+      alternativeText: getFileAttributes(image).alternativeText,
+      preview: previewImage,
+      large: largeImage,
+    };
+  });
+}
---
+++ src/lib/logger.ts
@@ -0,0 +1,12 @@
+import winston from "winston";
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: "logs/debug.log",
+    }),
+  ],
+});
