a8f99230f13dbb13260e056bc329af3497ad8ca1
David K <david@kagerer.co>
2024-05-12 19:53:55 +0000

    feat: add optimisation tools (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import icon from "astro-icon";
 import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import robotsTxt from "astro-robots-txt";
+import playformInline from "@playform/inline";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +18,11 @@ export default defineConfig({
       svgoOptions: {},
     }),
     astroImageTools,
-    robotsTxt({ sitemap: false }),
+    robotsTxt({
+      sitemap: false,
+    }),
+    playformInline(),
+    playformCompress(),
   ],
   output: "server",
   prefetch: {
@@ -26,3 +32,4 @@ export default defineConfig({
     mode: "middleware",
   }),
 });
+
